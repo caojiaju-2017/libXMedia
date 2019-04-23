95ef0c5571efaacd5609e25eef96b14ffd230b9a
caojiaju-2017 <jiaju_cao@hotmail.com>
2019-04-23 22:29:00 +0800

    init

---
+++ MyFFMPegProject/include/libmediax.h
@@ -0,0 +1,124 @@
+#ifndef  __LIBMEDIA_X__
+
+#define __LIBMEDIA_X__
+
+#include "public.h"
+
+/**************************************************
+@brief   : 初始化操作
+@author  : caojiaju/609853524@qq.com
+@time    : 2019/4/12
+@input   : 初始化成功，通过引用返回操作举兵  
+@return  : >= 0 表示成功 ； < 0 表示失败
+**************************************************/
+int InitHandle(int& operatorHandle);
+
+
+
+/**************************************************
+@brief   : 关闭清理
+@author  : caojiaju/609853524@qq.com
+@time    : 2019/4/12
+@input   : 之前初始化得到的句柄
+@return  :>= 0 表示成功 ； < 0 表示失败
+**************************************************/
+int CloseHandle(int& operatorHandle);
+
+
+
+/**************************************************
+@brief   : 打开资源，并播放
+@author  : caojiaju
+@time    : 2019/4
+@input1   : 资源地址
+@input2   : 播放视频的窗口句柄
+@input3   :  当前应用实例句柄
+@input4   :  输入的资源类型
+@return  : >= 0 表示成功 ； < 0 表示失败
+**************************************************/
+int OpenSource(const int& insHandle, const char* source, const int* wndPtr, SourceType sourcetype);
+
+
+/**************************************************
+@brief   : 接收流，并将流推送到指定流媒体服务器 --- 缺少格式指定
+@author  : caojiaju
+@time    : 2019/4
+@input1   :insHandle 句柄
+@input2   :媒体地址
+@input3   :转发/存地址
+@input4   :seconds  转发时长
+@return  : >= 0 表示成功 ； < 0 表示失败
+**************************************************/
+int ReMuxerLive(const int& insHandle,const char* source, const char* dest, int seconds);
+
+
+/**************************************************
+@brief   : 将流持久化 --- 将一路live，转存成文件
+@author  : caojiaju
+@time    : 2019/4
+@input1   :操作句柄
+@input2   :媒体地址---流
+@input3   :输出文件名
+@input4   :编码方式
+@input5   :转存时长，单位秒
+@return  : >= 0 表示成功 ； < 0 表示失败
+**************************************************/
+int PersientLive(const int& insHandle, const char* source, const char* filename, CodecType codecType, int seconds);
+
+
+/**************************************************
+@brief   : 终止上一次媒体操作
+@author  : caojiaju
+@time    : 2019/4
+@input   : 操作句柄
+@return  : >= 0 表示成功 ； < 0 表示失败
+**************************************************/
+int AbortLastOperator(const int& operatorHandle);
+
+/**************************************************
+@brief   : 暂停上一次媒体操作
+@author  : caojiaju
+@time    : 2019/4
+@input   : 操作句柄
+@return  : >= 0 表示成功 ； < 0 表示失败
+**************************************************/
+int PauseLastOperator(const int& operatorHandle);
+
+
+
+/**************************************************
+@brief   : 继续上一次媒体操作
+@author  : caojiaju
+@time    : 2019/4
+@input   : 操作句柄
+@return  : >= 0 表示成功 ； < 0 表示失败
+**************************************************/
+int ResumeLastOperator(const int& operatorHandle);
+
+
+
+/**************************************************
+@brief   : 上一次动作结束回调函数---必须在InitHandle调用成功后立即调用
+@author  : caojiaju
+@time    : 2019/4
+@input1   : 操作句柄
+@input2   : 回调函数
+@return  : >= 0 表示成功 ； < 0 表示失败
+**************************************************/
+int SetTaskFinishCallBack(const int& operatorHandle, void* funcPt);
+
+/**************************************************
+@brief   : 获取最近一次错误信息
+@author  : caojiaju
+@time    : 2019/4
+@input1   :操作句柄
+@input2   :错误代码
+@input3   :错误信息接受指针
+@return  :
+**************************************************/
+void getLastError(const int& operatorHandle,int errorcode, char* errorInfo);
+
+
+bool IsBusy(const int& operatorHandle);
+
+#endif // ! __LIBMEDIA_X__
---
+++ MyFFMPegProject/libmediaxTest/libmediaxTest.cpp
@@ -0,0 +1,68 @@
+﻿
+// libmediaxTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+#include "libmediax.h"
+#include <thread>
+#include "../libmediax/libmediax.h"
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+	
+	int* instance1 = new int(0);
+	std::cout << "Instance1=" << *instance1 << std::endl;
+	InitHandle(*instance1);
+	std::cout << "Instance1=" << *instance1 << std::endl;
+
+	int* instance2 = new int(0);
+	std::cout << "Instance2=" << *instance2 << std::endl;
+	InitHandle(*instance2);
+	std::cout << "Instance2=" << *instance2 << std::endl;
+
+
+	PersientLive(*instance1, "rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov", "d:\\Work\\Temp\\output1.mp4", H264Codec, 80);
+
+	/*std::chrono::milliseconds dura(6000);
+	std::this_thread::sleep_for(dura);
+
+	PersientLive(*instance2, "rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov", "d:\\Work\\Temp\\output2.flv", H264Codec, 30);*/
+
+
+	/// upload github
+	//答复是否撒旦
+	//std::chrono::milliseconds dura(6000);
+	//std::this_thread::sleep_for(dura);
+	while (true)
+	{
+		if (!IsBusy(*instance1))
+		{
+			CloseHandle(*instance1);
+			delete instance1;
+			break;
+		}
+
+		std::chrono::milliseconds dura(1000);
+		std::this_thread::sleep_for(dura);
+		
+	}
+
+	/*CloseHandle(*instance1);
+
+	CloseHandle(*instance2);*/
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
